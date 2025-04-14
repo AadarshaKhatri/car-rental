ec6124939e06ca13df8b58caeb2fe3a134fe55b9
Aadarsha Khatri <adarskhatri1@gmail.com>
2025-04-14 19:17:32 +0545

    feat: Monthly Trend Completed

--- app/(authenticated)/profile/components/LineChart/LineChart.tsx
+++ app/(authenticated)/profile/components/LineChart/LineChart.tsx
@@ -1,28 +1,39 @@
+"use client";
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import axios from "axios";
+import { useEffect, useState } from "react"
 import { Line,LineChart, ResponsiveContainer, XAxis, YAxis,Tooltip } from "recharts"
 
+interface LineGraphsTypes{
+  value:string,
+  month:string,
+}
 const LineGraph = () => {
-  const monthlyData = [
-    { month: "Jan", views: 120, bookings: 90 },
-    { month: "Feb", views: 200, bookings: 150 },
-    { month: "Mar", views: 280, bookings: 200 },
-    { month: "Apr", views: 330, bookings: 260 },
-    { month: "May", views: 420, bookings: 310 },
-    { month: "Jun", views: 470, bookings: 370 },
-  ]
+  const [data,setData] = useState<LineGraphsTypes []>();
+
+  useEffect(()=>{
+    async function FetchData(){
+      const {data} = await axios.get("/api/getLineChartMetrics")
+      setData(data)
+    }
+    FetchData();
+  },[])
+
+  console.log("Line Chart Data:",data);
+
   return (
     <Card className="col-span-2">
     <CardHeader>
-      <CardTitle>Monthly Trends</CardTitle>
+      <CardTitle>Monthly Booking Trends</CardTitle>
     </CardHeader>
     <CardContent className="h-[250px]">
       <ResponsiveContainer width="100%" height="100%">
-        <LineChart data={monthlyData}>
+        <LineChart data={data}>
           <XAxis dataKey="month" stroke="#999" />
           <YAxis stroke="#999" />
-          <Tooltip />
-          <Line type="monotone" dataKey="views" stroke="#4ade80" strokeWidth={2} />
-          <Line type="monotone" dataKey="bookings" stroke="#60a5fa" strokeWidth={2} />
+          <Tooltip labelClassName="text-gray-800"/>
+          <Line type="bumpX" dataKey="no_of_Booking" stroke="#60a5fa" strokeWidth={2} />
         </LineChart>
       </ResponsiveContainer>
     </CardContent>
